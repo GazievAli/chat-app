852d64cac635f3c570ba95a5515821b9486a9762
AliGaziev <gaziev.80is@gmail.com>
2024-11-16 22:28:40 +0300

    end

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Chat from "./components/chat/Chat";
+import Detail from "./components/chat/Detail";
+import List from "./components/chat/List";
+
 const App = () => {
   return (
     <div className='container'>
-      
+      <List />
+      <Chat />
+      <Detail />
     </div>
   )
 }
---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,9 @@
+import "./Chat.css"
+
+const Chat = () => {
+    return (
+        <div className="chat">
+
+        </div>
+    )
+}
